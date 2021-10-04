65be8bd7b53b77a433dabf4be3e2ef9dbdb5c371
Ray Blair <ray_blair@epam.com>
2021-10-04 20:46:04 +0100

    Task1-3

--- task1.js
+++ task1.js
@@ -1,22 +1,3 @@
-const reader = process.stdin;
+const { reverseStdIn } = require("./src/task1.js");
 
-// Reverse our string and remove any newlines
-function transform(value) {
-  let transformed = value
-    .reverse()
-    .toString()
-    .split(/[\r\n|\n]/)
-    .pop();
-
-  output(
-    transformed
-  );
-}
-
-// Output our value with newlines
-function output(value) {
-  console.log(value + "\n\n");
-}
-
-// When 'data' event is ran, transform and output
-reader.on("data", transform);
+reverseStdIn();
