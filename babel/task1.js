dd1abd079b3de9ee08e4168ca121db4d3de11605
Ray Blair <ray_blair@epam.com>
2021-10-11 21:56:56 +0100

    WIP

---
+++ babel/task1.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function command() {
+  process.stdin.on("data", function (data) {
+    // Reverse our string and remove any newlines
+    var string = data.reverse().toString().split(/[\r\n|\n]/).pop();
+
+    // Return with newline
+    console.log(string + "\n\n");
+  });
+}
+
+module.exports = { command: command };
