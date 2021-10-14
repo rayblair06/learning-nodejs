c08386221ffbfa6719a5acf36e2a23844268cc29
Ray Blair <ray_blair@epam.com>
2021-10-14 17:02:51 +0100

    Requested changes

--- task2.js
+++ task2.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const csv = require("csvtojson");
 const path = require("path");
+const { pipeline } = require("stream");
 
 // Setup Read/Write paths
 const readPath = path.join(__dirname, "./csv/file.csv");
@@ -11,15 +12,14 @@ const writePath = path.join(__dirname, "./file.txt");
 const reader = fs.createReadStream(readPath);
 const writer = fs.createWriteStream(writePath);
 
-reader.on('data', (chunk) => {
-  // Convert chunk string to JSON then write to file
+// Convert string to JSON
+const transform = 
   csv()
-    .fromString(chunk.toString())
     .subscribe(
       (json, lineNumber) => {
         try {
-          writer.write(JSON.stringify(json) + "\n");
-        } catch(error) {
+          return JSON.stringify(json) + "\n";
+        } catch (error) {
           console.error(error);
         }
       },
@@ -27,5 +27,20 @@ reader.on('data', (chunk) => {
         console.error(error);
       }
     );
-})
 
+    // Our callback function
+const callback = (error) => {
+  if (error) {
+    console.error('Pipeline failed.', error);
+  } else {
+    console.log('Pipeline succeeded.');
+  }
+}
+
+// Run our pipeline
+pipeline(
+  reader,
+  transform,
+  writer,
+  callback
+);
